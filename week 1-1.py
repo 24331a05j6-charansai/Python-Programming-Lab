139f30089c22fd48a7ffc0ebaeac85bb1045ab09
24331a05j6-charansai <24331a05j6@mvgrce.edu.in>
2025-12-14 21:13:19 +0530

    Add files via upload

---
+++ week 1-1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter the value of a:"))
+print(a)
+b=float(input("Enter the value of b:"))
+print(b)
+char_a='A'
+print(char_a)
+name="charan"
+print(name)
+print(type(a))
+print(type(b))
+print(type(char_a))
+print(type(name))
---
+++ week 1-4.py
@@ -0,0 +1,12 @@
+import math
+a=math.pi
+print(a)
+b=int(input("Enter the value of b:"))
+result=math.sqrt(b)
+print(result)
+angle=int(input("Enter the angle:"))
+radian=math.radians(angle)
+x=math.sin(radian)
+print(x)
+y=math.cos(radian)
+print(y)
